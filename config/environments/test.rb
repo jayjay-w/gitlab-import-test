6193c7c441bf608549a8a8453b03fe5ace5d81de
Joseph Joshua <jjoshua@gitlab.com>
2020-10-05 16:51:41 +0300

    Add allowed hosts

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -24,7 +24,7 @@ Rails.application.configure do
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = false
   config.cache_store = :null_store
-
+  config.hosts << "jayjoshua-todo.sr.gitlab.support"
   # Raise exceptions instead of rendering exception templates.
   config.action_dispatch.show_exceptions = false
 
